013866bc31eab533cf1e01de4b8dcf5b6bd09809
steinko <stein_korsveien@msn.com>
2020-12-22 03:57:06 +0100

    Gui with Ammount field and Whitdra butto

--- src/AutomaticTeller/AutomaticTeller.jsx
+++ src/AutomaticTeller/AutomaticTeller.jsx
@@ -2,5 +2,30 @@ import React from 'react';
 import {Component} from 'react';
 
 export default class AutomaticTeller extends Component{
-	                render() {return <div></div>}
+	
+	                constructor(props){
+		              super(props)
+		              this.state = {amount: ''}
+                      this.handleChange = this.handleChange.bind(this)
+                      this.handleSubmit = this.handleSubmit.bind(this)
+	                }
+
+                    handleChange(event){
+	                        this.setState({amount: event.target.value})
+                    }
+
+                    handleSubmit(event){
+                          event.preventDefault()	
+                    }
+
+	                render() {return ( <div>
+                                         <form onSubmit={this.handleSubmit}>
+                                           <label>
+                                              Amount:
+                                              <input id = "amount" type="text" name="name" value= {this.state.amount} onChange= {this.handleChange} />
+                                           </label>
+                                           <input type="submit" id = "withdraw" value="Withdraw" />
+                                         </form>
+                                       </div>)
+                                    }
                }
--- src/AutomaticTeller/AutomaticTeller.test.js
+++ src/AutomaticTeller/AutomaticTeller.test.js
@@ -3,7 +3,36 @@ import { render, fireEvent } from '@testing-library/react';
 import  AutomaticTeller from './AutomaticTeller';
 
 
-it ('should exist', async  () => { 
+it ('should exist', async  () => {
+ 
     let  { getByText} = render(<AutomaticTeller />)
-   await  expect(getByText).not.toBeNull()
+    await  expect(getByText).not.toBeNull()
+   
+})
+
+
+it ('should display a lable Amount:',  () => { 
+
+    let { queryByLabelText } = render (<AutomaticTeller />)
+    expect(queryByLabelText('Amount:')).toBeInTheDocument()
+    
+})
+
+it ('should display submit buttom withdraw',  () => { 
+
+    let { queryByText } = render (<AutomaticTeller />)
+    expect(queryByText('Withdraw')).toBeInTheDocument()
+    
+})
+
+it ('should display entered amount',  () => { 
+
+   let { queryByLabelText, queryByText} = render(<AutomaticTeller />)
+   let amount = '100'
+   let amountLable = queryByLabelText('Amount:')
+   fireEvent.change(amountLable, { target: { value:  amount } })
+   expect(amountLable.value).toBe(amount) 
+   fireEvent.click(queryByText("Withdraw"))
+   expect(amountLable.value).toBe(amount)
+   
 })
